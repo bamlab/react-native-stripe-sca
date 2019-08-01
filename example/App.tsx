1a9fb92ba09c42f1a7a23fd356ffb0ba58f8d81d
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-08-01 21:26:25 +0200

    :bug: (Example) Fix App import

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { View, Text } from 'react-native';
 
-const App = () => {
+export const App = () => {
   return (
     <View>
       <Text>Welcome in React Native Stripe SCA</Text>
