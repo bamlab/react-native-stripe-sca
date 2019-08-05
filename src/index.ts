d2bee3fa0fdae472a58363b0bb2c4662be5271cf
AntoineDoubovetzky <antoined@bam.tech>
2019-08-05 21:28:29 +0200

    :sparkles: (src/index) create Stripe class with setPublishableKey method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { NativeModules } from 'react-native';
 
-export const sampleMethod = () => {
-  NativeModules.StripeSca.sampleMethod('test', 12, console.log);
-};
+class StripeModule {
+  private constructor() {}
+  static _instance: StripeModule;
+  static get instance() {
+    if (!StripeModule._instance) {
+      StripeModule._instance = new StripeModule();
+    }
+    return StripeModule._instance;
+  }
+
+  setPublishableKey = (publishableKey: string) =>
+    NativeModules.StripeSca.setPublishableKey(publishableKey);
+}
+
+export const Stripe = StripeModule.instance;
