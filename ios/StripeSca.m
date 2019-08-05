d9e8798bc2298d2a16f5eebe4ac07219f6a0c967
AntoineDoubovetzky <antoined@bam.tech>
2019-08-05 21:27:44 +0200

    :green_apple: (StripeSca) add setPublishableKey method

--- ios/StripeSca.m
+++ ios/StripeSca.m
@@ -3,23 +3,22 @@
 
 @implementation StripeSca
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
 
-- (id)init {
-    self = [super init];
-    [[STPPaymentConfiguration sharedConfiguration] setPublishableKey:@"pk_test_NGDSl1RJCR0kwCvsk2gSvNtS"];
-    return self;
-}
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(setPublishableKey:(NSString *)publishableKey
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    // TODO: Implement some actually useful functionality
-	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    NSLog(@"setPublishableKey");
+    @try {
+        [[STPPaymentConfiguration sharedConfiguration] setPublishableKey:publishableKey];
+        resolve(@YES);
+    }
+    @catch (NSError * error) {
+        reject(@"1", @"Error setting publishable key", error);
+    }
 }
 
 @end
