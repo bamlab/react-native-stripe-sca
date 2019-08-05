3fd29940f271dcd40676478cedd159f75e3d361c
AntoineDoubovetzky <antoined@bam.tech>
2019-08-05 22:03:44 +0200

    :white_check_mark: (example) test setPublishableKey manually in app

--- example/App.tsx
+++ example/App.tsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import { View, Text } from 'react-native';
-import { sampleMethod } from 'react-native-stripe-sca';
+import { Stripe } from 'react-native-stripe-sca';
 
 export const App = () => {
-  sampleMethod();
+  Stripe.setPublishableKey('whatever')
+    .then(console.log)
+    .catch(console.warn);
   return (
     <View>
       <Text>Welcome in React Native Stripe SCA</Text>
